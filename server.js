ff842429641bfcf1dbc7057dcde457b8686690b0
profeguille <guillermofergnani@gmail.com>
2022-09-24 14:15:08 -0300

    session test

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+//const cookieParser = require("cookie-parser");
+const session = require("express-session");
+
+const app = express();
+//app.use(cookieParser("el secreto"));
+app.use(
+  session({
+    secret: "el secreto",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.get("/login", (req, res) => {
+  const { user, pas } = req.query;
+  if (user !== "pepe" || pas !== "asdasd") {
+    return res.send("login failed");
+  } else {
+    req.session.user = user;
+    req.session.admin = true;
+    return res.send("login success!");
+  }
+});
+
+app.get("/test", (req, res) => {
+  console.log(req.session.user);
+  console.log(req.session.admin);
+  res.send("ruta test");
+});
+
+function checkAdmin(req, res, next) {
+  if (!req.session.admin) return res.status(403).send("ACA NO!");
+  return next();
+}
+
+//RUTAS PARA ADMIN
+app.get("/algomuyimportante", checkAdmin, (req, res) => {
+  return res.send("reportes ultra secretos de ventas truchas sin boleta");
+});
+
+app.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.json({ status: "Logout ERROR", body: err });
+    }
+    res.send("Logout ok!");
+  });
+});
+
+// app.get("/crearcookie", (req, res) => {
+//   return res
+//     .cookie("nombre", "guille", { signed: true, httpOnly: true })
+//     .send("<h1>GUARDAMOS TU COKIE</h1>");
+// });
+
+// app.get("/recuperarcookie", (req, res) => {
+//   console.log("///////////////////////////////");
+//   console.log(req.cookies);
+//   console.log(req.signedCookies);
+//   return res.send("<h1>mira la consola para ver si hay cookies</h1>");
+// });
+
+app.listen(8000, () => {
+  console.log(`Example app listening on port 8000`);
+});
